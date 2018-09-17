88c286a1ae3f4c68ecef202ebc01076d2a024354
MiniMarker <cfmarker@gmail.com>
2018-09-17 11:55:54 +0200

    added glennbech as user

--- github_team.tf
+++ github_team.tf
@@ -2,9 +2,17 @@ resource "github_team" "theclass" {
   name        = "pgr301class"
   description = "The students and teacher of the class"
 }
+
 resource "github_team_membership" "minimarker" {
   team_id  = "${github_team.theclass.id}"
   username = "minimarker"
+  role     = "member"
+}
+
+
+resource "github_team_membership" "glennbech" {
+  team_id  = "${github_team.theclass.id}"
+  username = "glennbech"
   role     = "member"
 }
 
